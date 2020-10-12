b8d64140e36ac6e4b6c48475dad5f4e8b77286bb
Carlos Roberto De Rolt Junior <carlosroltjr@gmail.com>
2020-10-11 21:17:57 -0300

    estudo sobre layouts

---
+++ app/src/main/java/com/example/senai_mobile/LinaerLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.senai_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinaerLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linaer_layout);
+    }
+}
